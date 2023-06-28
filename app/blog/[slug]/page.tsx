8ef32489abd3dde74e9f431307325ff2c6c8f48f
nekok500 <53959791+nekok500@users.noreply.github.com>
2023-06-28 12:15:02 +0900

    update: 試しにSpaceXのデータを上げてみる

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,66 +1,56 @@
-"use server";
-export const runtime = "edge";
+"use client";
 
-import { notFound } from "next/navigation";
 import { getClient } from "@/app/lib/client";
 import { gql } from "@apollo/client";
 
-export async function generateStaticParams() {
-  "use server";
+export const runtime = "edge";
 
-  const resp = await getClient().query<{
-    posts: {
-      nodes: {
-        slug: string;
-      }[];
+export async function generateStaticParams() {
+  const client = getClient();
+  const { data } = await client.query<{
+    launches: {
+      id: string;
+      mission_name: string;
+    }[];
+  }>({
+    query: gql`
+      query GetLaunches {
+        launches(limit: 100) {
+          id
+        }
+      }
+    `,
+  });
+
+  return data.launches.map((launch) => {
+    slug: launch.id;
+  });
+}
+
+export default async function Page() {
+  const client = getClient();
+  const { data } = await client.query<{
+    launch: {
+      mission_name: string;
+      details: string;
     };
   }>({
     query: gql`
-      query Posts {
-        posts {
-          nodes {
-            slug
-          }
+      query GetLaunches($id: ID!) {
+        launch(id: $id) {
+          mission_name
+          details
         }
       }
     `,
   });
-
-  return resp.data.posts.nodes.map((post) => ({ slug: post.slug }));
-}
-
-export const revalidate = 5;
-
-interface PostResult {
-  post: {
-    title: string;
-    slug: string;
-  };
-}
-
-export default async function Page({ params }: { params: { slug: string } }) {
-  const client = getClient();
-  const { data } = await client.query<PostResult>({
-    query: gql`
-      query Post($slug: ID!) {
-        post(idType: SLUG, id: $slug) {
-          title
-        }
-      }
-    `,
-    variables: {
-      slug: params.slug,
-    },
-    context: {},
-  });
-
-  if (!data.post) notFound();
 
   return (
-    <main>
-      <div className="mx-auto py-6">
-        <h2 className="text-gray-900 text-3xl font-bold">{data.post.title}</h2>
-      </div>
-    </main>
+    <div className="mx-auto py-6">
+      <h1 className="text-4xl font-bold text-gray-900">
+        {data.launch.mission_name}
+      </h1>
+      <p className="mt-2 leading-6">{data.launch.details}</p>
+    </div>
   );
 }
