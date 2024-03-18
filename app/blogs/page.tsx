536979cd17eba043a2033d140d7bf8b71e51fa4d
nekok500 <me@nekok500.com>
2024-03-19 03:27:56 +0900

    update: ブログページのタイトルを追加

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,8 +1,13 @@
 import { Blog, client } from "@/libs/microcms";
 import BlogList from "./_components/list";
+import { Metadata } from "next";
 
 export const revalidate = 60;
 export const runtime = "edge";
+
+export const metadata: Metadata = {
+  title: "ブログ",
+};
 
 export default async function BlogListPage() {
   const { contents } = await client.getList<Blog>({ endpoint: "blogs" });
