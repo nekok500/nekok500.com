1c44b728d9bd3fe10c214ecb1459d6cac6130af3
nekok500 <me@nekok500.com>
2024-03-20 01:43:13 +0900

    fix: add default

--- app/test/refresh/page.tsx
+++ app/test/refresh/page.tsx
@@ -2,7 +2,7 @@ import { revalidateTag } from "next/cache";
 
 export const runtime = "edge";
 
-export function Page() {
+export default function Page() {
   revalidateTag("test");
 
   return <span>purged</span>;
