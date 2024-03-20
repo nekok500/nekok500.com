b283c00fe7249203335fb66e657087ef05e319ee
nekok500 <me@nekok500.com>
2024-03-20 15:07:50 +0900

    update: 時刻をフォーマッターを使うように

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,9 +1,13 @@
 export function toDateString(date: Date) {
-  return `${date.getFullYear()}年 ${date.getMonth() + 1}月
-  ${date.getDate()}日 ${date.getHours().toString().padStart(2, "0")}時${date
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}分`;
+  const formatter = new Intl.DateTimeFormat(["ja-JP"], {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return formatter.format(date);
 }
 
 export function getSlug(slug: string): { id: string; date?: string } {
