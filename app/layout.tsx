18e5e61bc139c2edee23626f996809597e76febc
ねこかわいい <me@nekok500.com>
2024-04-06 02:13:24 +0900

    tweaks: html lang=jaに

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body className={inter.className}>
         <NextTopLoader />
         <Providers>
