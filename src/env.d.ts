b08e249cca26c1849cc2bd141b801c43803784bb
nekok500 <nekok500@proton.me>
2024-11-18 02:45:58 +0900

    add: blog

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly MICROCMS_SERVICE_DOMAIN: string
+  readonly MICROCMS_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import type { MicroCMSContentId, MicroCMSDate } from 'microcms-js-sdk'
+
+export function yyyyMMdd(date: Date): string {
+  return date.toISOString().substring(0, 10).replace(/-/g, '')
+}
+
+export function toSlug(obj: MicroCMSContentId & MicroCMSDate) {
+  const jst = new Date(new Date(obj.publishedAt!).getTime() + 9 * 3600 * 1000) // +9時間
+  return `${yyyyMMdd(jst)}-${obj.id}`
+}
+
+export function fromSlug(slug: string) {
+  return slug.substring(9)
+}
